6fbc03aff67b5b20792488c5ec732c0447d421c4
David Ko <david.ko@pvtmethod.com>
2017-10-27 17:00:21 -0400

    Broken main.go

--- main.go
+++ main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
 	"github.com/spf13/afero"
+	"gopkg.in/labstack/echo.v3"
 )
 
-func main() {
-	AppFs := afero.NewOsFs()
-	files, _ := afero.Glob(AppFs, "files/*")
+// func main() {
+// AppFs := afero.NewOsFs()
+// files, _ := afero.Glob(AppFs, "files/*")
+//
+// for i := 0; i < len(files); i++ {
+// 	fmt.Println(files[i])
+// }
+// }
 
-	for i := 0; i < len(files); i++ {
-		fmt.Println(files[i])
-	}
+func main() {
+	// e := echo.New()
+	// e.Get("/", func(ctx echo.Context) error {
+	// 	return ctx.String(http.StatusOK, "Hello, World!")
+	// })
+	// e.Logger.Fatal(e.Start(":1323"))
 }
