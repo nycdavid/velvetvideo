5ae6f9ad1291013370000ce6842e7b2d27948717
David Ko <david.ko@damncarousel.com>
2017-11-04 08:39:41 -0400

    Display videos by rendering templates [Fixes #20] (#25)

--- main.go
+++ main.go
@@ -3,25 +3,41 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"html/template"
+	"io"
 	"net/http"
-	"strconv"
+	"net/url"
 
 	"github.com/fatih/color"
-	"gopkg.in/labstack/echo.v2"
-	"gopkg.in/labstack/echo.v2/engine"
-	"gopkg.in/labstack/echo.v2/engine/standard"
+	"github.com/spf13/afero"
+	"gopkg.in/labstack/echo.v3"
 )
+
+type HomeView struct {
+	templates *template.Template
+}
+
+func (h *HomeView) Render(w io.Writer, name string, data interface{}, ctx echo.Context) error {
+	htmlTmpl, err := template.New("home.html").ParseFiles("src/templates/home.html")
+	if err != nil {
+		fmt.Println("Error parsing template string")
+	}
+	return htmlTmpl.ExecuteTemplate(w, name, data)
+}
 
 func main() {
 	e := echo.New()
+	e.Renderer = &HomeView{}
 	e.Use(VelvetLogger)
 	e.GET("/", Home)
 	e.Static("/dist", "dist")
-	e.Run(standard.New(":1323"))
+	e.Start(":1323")
 }
 
 func Home(ctx echo.Context) error {
-	return ctx.String(http.StatusOK, "Homepage")
+	AppFs := afero.NewOsFs()
+	files, _ := afero.Glob(AppFs, "files/*")
+	return ctx.Render(http.StatusOK, "home.html", files)
 }
 
 func VelvetLogger(next echo.HandlerFunc) echo.HandlerFunc {
@@ -29,15 +45,15 @@ func VelvetLogger(next echo.HandlerFunc) echo.HandlerFunc {
 		req := ctx.Request()
 		res := ctx.Response()
 		var buf bytes.Buffer
-		buf.WriteString(req.Method())
+		buf.WriteString(req.Method)
 		buf.WriteString(" ")
-		fullUrl := parseReqUrl(req.URL())
-		buf.WriteString(fullUrl)
-		statuscode := strconv.Itoa(res.Status())
+		// fullUrl := parseReqUrl(req.URL.Path)
+		// buf.WriteString(fullUrl)
+		statuscode := res.Status
 		buf.WriteString(" ")
 		fmt.Print(buf.String())
-		if statuscode == "200" {
-			color.Green("%s %s", statuscode, http.StatusText(res.Status()))
+		if statuscode == 200 {
+			color.Green("%s %s", statuscode, http.StatusText(res.Status))
 		} else {
 		}
 
@@ -46,12 +62,12 @@ func VelvetLogger(next echo.HandlerFunc) echo.HandlerFunc {
 	}
 }
 
-func parseReqUrl(url engine.URL) string {
+func parseReqUrl(url *url.URL) string {
 	var buf bytes.Buffer
-	buf.WriteString(url.Path())
-	if url.QueryString() != "" {
-		buf.WriteString("?")
-		buf.WriteString(url.QueryString())
-	}
+	// buf.WriteString(url.Path())
+	// if url.QueryString() != "" {
+	// 	buf.WriteString("?")
+	// 	buf.WriteString(url.QueryString())
+	// }
 	return buf.String()
 }
